440a2fbf156b9155aa3df3a25e7c02604e934469
Dana <kajratdana352@gmail.com>
2026-03-28 11:08:54 +0500

    lab9

---
+++ lab9/shop_back/api/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from .views import CategoryViewSet, ProductViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'categories', CategoryViewSet)
+router.register(r'products', ProductViewSet)
+
+
+urlpatterns = [
+    path('', include(router.urls))
+]
